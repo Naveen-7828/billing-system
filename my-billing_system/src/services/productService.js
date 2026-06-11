ec1d175d0f5e82c626e60f991034582ee7e19432
Naveen-7828 <naveen.mj2.45@gmail.com>
2026-06-11 14:45:19 +0530

    Fix API endpoints and add CORS configuration for deployment

--- my-billing_system/src/services/productService.js
+++ my-billing_system/src/services/productService.js
@@ -1,6 +1,6 @@
  import axios from "axios";
 
- const API = import.meta.env.VITE_API_URL + "/api/products";
+ const API = import.meta.env.VITE_API_URL + "/api/product";
 
 export const getProducts = () => axios.get(API);
 export const createProduct = (data) => axios.post(API, data);
